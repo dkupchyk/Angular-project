15373c7113839c9356828ec539fca7e5df432e91
Daryna Kupchyk <dasha.kupchik28@gmail.com>
2020-07-23 20:38:24 +0300

    Initialized form in contact component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import {RouterModule, Routes} from '@angular/router';
 import {NgModule} from '@angular/core';
+import {HomeComponent} from './home/home.component';
+import {ContactComponent} from './shared/contact/contact.component';
 
-const appRoutes: Routes = [];
+const appRoutes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full' },
+  {path: 'home', component: HomeComponent},
+  {path: 'contact', component: ContactComponent}
+];
 
 @NgModule({
   imports: [
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {AppRoutingModule} from './app-routing.module';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { OtherComponent } from './shared/other/other.component';
+import { ContactComponent } from './shared/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    OtherComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
