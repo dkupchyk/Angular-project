1897711f164a5846c564ef29d8b4a230d65a1823
Daryna Kupchyk <dasha.kupchik28@gmail.com>
2020-08-07 18:40:39 +0300

    Created progress icons

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,17 +4,19 @@ import {ReactiveFormsModule} from '@angular/forms';
 import {AppRoutingModule} from '../app-routing.module';
 import {AuthComponent} from './auth.component';
 import {SignUpComponent} from './sign-up/sign-up.component';
+import {SharedModule} from "../shared/shared.module";
 
 @NgModule({
   declarations: [
     AuthComponent,
     SignUpComponent
   ],
-  imports: [
-    BrowserModule,
-    ReactiveFormsModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        ReactiveFormsModule,
+        AppRoutingModule,
+        SharedModule
+    ],
   exports: [
     AuthComponent,
     SignUpComponent
--- src/app/auth/sign-up/sign-up.component.ts
+++ src/app/auth/sign-up/sign-up.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, NgForm, Validators} from "@angular/forms";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-sign-up',
@@ -6,10 +8,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-up.component.less']
 })
 export class SignUpComponent implements OnInit {
+  signUpForm: FormGroup;
+  error: string = null;
+  sectionsIcons: string[] = [
+    '../../assets/icons/1.svg',
+    '../../assets/icons/2.svg',
+    '../../assets/icons/3.svg'
+  ];
+  section = 1;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder,
+              private router: Router) {
+  }
 
   ngOnInit(): void {
+    this.initForm();
+  }
+
+  initForm(): void {
+    this.signUpForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+
+      dateOfBirth: ['', Validators.required],
+
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+
+  onHandleError(): void {
+    this.error = null;
+  }
+
+  onSubmit(form: NgForm): void {
+    form.reset();
+  }
+
+  changeToSignUpView(): void {
+    this.router.navigate(['sign-up']);
+  }
+
+  nextSection() {
+    this.section++;
   }
 
 }
